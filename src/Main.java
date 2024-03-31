f62d950826047a14b3927e1da68c0ef6639b6673
Abdibaitov <91374399+Abdibaitov@users.noreply.github.com>
2024-03-31 17:50:48 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,16 @@ public class Main {
         String name= scanner.nextLine();
         System.out.println("Your name is: "+name);
 
-        sum(4,5);
-
+        minus(5,1);
+        koboytuu(4,4);
     }
 
-    public static void sum(int a, int b){
-        System.out.println(a+b);
+    public static void minus(int a, int b){
+        System.out.println(a-b);
     }
+
+    public static void koboytuu(int a, int v){
+        System.out.println(a*v);
+    }
+
 }
