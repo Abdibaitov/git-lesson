db51086a393b610eb733500afdd8a8ba433d67a6
Abdibaitov <91374399+Abdibaitov@users.noreply.github.com>
2024-03-31 16:59:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please write your age:");
+        int a = scanner.nextInt();
+        System.out.println("Your age is:"+a);
+    }
+}
