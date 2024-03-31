2ce18df5710ed5ca3712692ede48819f8fe0ca80
Abdibaitov <91374399+Abdibaitov@users.noreply.github.com>
2024-03-31 17:06:08 +0300

    file is changed 1

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Please write your age:");
-        int a = scanner.nextInt();
-        System.out.println("Your age is:"+a);
+        System.out.println("Hello world!");
     }
 }
